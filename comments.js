dc031954d456dd024f30ee9e380193473ec54ec7
Nishit Agarwal <84432753+Nishit0409@users.noreply.github.com>
2023-07-24 16:58:09 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,30 @@
+// create web server
+// $ npm install -g http-server
+// $ http-server
+// http://localhost:8080/app/components/member/member.html
+// http://localhost:8080/app/components/member/member.js
+// http://localhost:8080/app/components/member/member.controller.js
+// http://localhost:8080/app/components/member/member.css
+// http://localhost:8080/app/components/member/member.spec.js
+// http://localhost:8080/app/components/member/member.service.js
+// http://localhost:8080/app/components/member/member.directive.js
+// http://localhost:8080/app/components/member/member.filter.js
+// http://localhost:8080/app/components/member/member.module.js
+// http://localhost:8080/app/components/member/member.constant.js
+// http://localhost:8080/app/components/member/member.value.js
+// http://localhost:8080/app/components/member/member.factory.js
+// http://localhost:8080/app/components/member/member.provider.js
+// http://localhost:8080/app/components/member/member.decorator.js
+// http://localhost:8080/app/components/member/member.animation.js
+// http://localhost:8080/app/components/member/member.config.js
+// http://localhost:8080/app/components/member/member.route.js
+// http://localhost:8080/app/components/member/member.run.js
+// http://localhost:8080/app/components/member/member.test.js
+// http://localhost:8080/app/components/member/member.mock.js
+// http://localhost:8080/app/components/member/member.mock.json
+// http://localhost:8080/app/components/member/member.mock.xml
+// http://localhost:8080/app/components/member/member.mock.csv
+// http://localhost:8080/app/components/member/member.mock.yaml
+// http://l
+
+
